d0455df855ab95569e78db8fa6b94fe224b0c4ca
Вячеслав <smole17@mail.ru>
2023-08-17 00:23:34 +0300

    fixed: 3d cube

--- src/main/kotlin/ru/dargen/evoplus/api/render/node/CubeNode.kt
+++ src/main/kotlin/ru/dargen/evoplus/api/render/node/CubeNode.kt
@@ -1,19 +1,23 @@
 package ru.dargen.evoplus.api.render.node
 
+import com.mojang.blaze3d.systems.RenderSystem
 import net.minecraft.client.util.math.MatrixStack
 import ru.dargen.evoplus.util.kotlin.KotlinOpens
 import ru.dargen.evoplus.util.math.v3
 import ru.dargen.evoplus.util.render.drawCube
+import ru.dargen.evoplus.util.render.translate
 
 @KotlinOpens
 class CubeNode : Node() {
 
     init {
-        size = v3(40.0, 40.0, 40.0)
+        size = v3(1.0, 1.0, 1.0)
     }
 
     override fun renderElement(matrices: MatrixStack, tickDelta: Float) {
+        if (!isSeeThrough) RenderSystem.enableDepthTest()
         matrices.drawCube(size, color.rgb)
+        if (!isSeeThrough) RenderSystem.disableDepthTest()
     }
 
 }
