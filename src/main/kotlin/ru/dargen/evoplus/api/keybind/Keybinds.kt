11b403e9e3de10bed85c2f67ff9ee7a66bfc9636
Вячеслав <smole17@mail.ru>
2023-12-12 18:36:24 +0300

    removed funny thing

--- src/main/kotlin/ru/dargen/evoplus/api/keybind/Keybinds.kt
+++ src/main/kotlin/ru/dargen/evoplus/api/keybind/Keybinds.kt
@@ -6,6 +6,5 @@ object Keybinds {
 
     val MenuKey = keyBind("Открыть меню", InputUtil.GLFW_KEY_RIGHT_SHIFT, "EvoPlus")
     val FastSelector = keyBind("Окно быстрого доступа", InputUtil.GLFW_KEY_R, "EvoPlus")
-    val Clicker = keyBind("Авто-кликер", InputUtil.GLFW_KEY_X, "EvoPlus")
 
 }
