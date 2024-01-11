dc12a76f212451e8654351e075be57e7a4a44af5
dargen <dargencode@mail.ru>
2024-01-11 18:23:42 +0300

    rotate golden cristal item

--- src/main/kotlin/ru/dargen/evoplus/features/game/GoldenRushFeature.kt
+++ src/main/kotlin/ru/dargen/evoplus/features/game/GoldenRushFeature.kt
@@ -1,9 +1,12 @@
 package ru.dargen.evoplus.features.game
 
+import net.minecraft.client.render.DiffuseLighting
 import net.minecraft.entity.decoration.ArmorStandEntity
 import net.minecraft.item.Items
 import ru.dargen.evoplus.api.render.node.box.hbox
 import ru.dargen.evoplus.api.render.node.item
+import ru.dargen.evoplus.api.render.node.postTransform
+import ru.dargen.evoplus.api.render.node.preTransform
 import ru.dargen.evoplus.api.render.node.text
 import ru.dargen.evoplus.api.schduler.scheduleEvery
 import ru.dargen.evoplus.feature.Feature
@@ -34,7 +37,14 @@ object GoldenRushFeature : Feature("golden-rush", "Золотая Лихорад
             indent = v3()
             space = 3.0
 
-            +item(GoldenCristalItem) { scale = v3(.5, .5, .5) }
+            +item(GoldenCristalItem) {
+                scale = scale(.5, .5, .5)
+                rotation = v3(y = 50.0)
+                translation = v3(x = 12.0)
+
+                preTransform { matrices, tickDelta -> DiffuseLighting.disableGuiDepthLighting() }
+                postTransform { matrices, tickDelta -> DiffuseLighting.enableGuiDepthLighting() }
+            }
             +GoldenCrystalIndicatorText
         }
     }
