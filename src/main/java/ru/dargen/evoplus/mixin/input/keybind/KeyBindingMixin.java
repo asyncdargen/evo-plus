b6c23d6cef1832cfceda935fba450b53bde1f7d4
dargen <dargencode@mail.ru>
2024-01-16 08:08:20 +0300

    fix keybindings & boss bar hud mixin

--- src/main/java/ru/dargen/evoplus/mixin/input/keybind/KeyBindingMixin.java
+++ src/main/java/ru/dargen/evoplus/mixin/input/keybind/KeyBindingMixin.java
@@ -3,6 +3,7 @@ package ru.dargen.evoplus.mixin.input.keybind;
 import lombok.val;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.InputUtil.Type;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,8 +12,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import ru.dargen.evoplus.api.render.context.ScreenContext;
+import ru.dargen.evoplus.util.minecraft.MinecraftKt;
 
 import java.util.*;
+
+import static net.minecraft.client.util.InputUtil.isKeyPressed;
 
 @Mixin(KeyBinding.class)
 public class KeyBindingMixin {
@@ -20,6 +24,10 @@ public class KeyBindingMixin {
     @Shadow
     @Final
     private static Map<String, KeyBinding> KEYS_BY_ID;
+    @Shadow
+    @Final
+    private static Map<InputUtil.Key, KeyBinding> KEY_TO_BINDINGS;
+    @Shadow private boolean pressed;
     @Unique
     private static final Map<InputUtil.Key, List<KeyBinding>> KEY_BINDINGS = new HashMap();
 
@@ -28,7 +36,7 @@ public class KeyBindingMixin {
     }
 
     @Inject(method = "<init>(Ljava/lang/String;Lnet/minecraft/client/util/InputUtil$Type;ILjava/lang/String;)V", at = @At("TAIL"))
-    private void init(String translationKey, InputUtil.Type type, int code, String category, CallbackInfo ci) {
+    private void init(String translationKey, Type type, int code, String category, CallbackInfo ci) {
         putKey((KeyBinding) (Object) this);
     }
 
@@ -40,6 +48,8 @@ public class KeyBindingMixin {
     @Inject(method = "onKeyPressed", at = @At("TAIL"))
     private static void onKeyPressed(InputUtil.Key key, CallbackInfo ci) {
         KEY_BINDINGS.getOrDefault(key, Collections.emptyList()).forEach(bind -> {
+            if (KEY_TO_BINDINGS.get(key) == bind) return;
+
             var accessor = ((KeyBindingAccessor) bind);
             accessor.setTimesPressed(accessor.getTimesPressed() + 1);
         });
@@ -53,11 +63,27 @@ public class KeyBindingMixin {
         }
     }
 
+    @Inject(method = "updatePressedStates", at = @At("TAIL"))
+    private static void updatePressedStates(CallbackInfo ci) {
+        for (KeyBinding keyBinding : KEYS_BY_ID.values()) {
+            var accessor = (KeyBindingAccessor) keyBinding;
+            if (accessor.getBoundKey().getCategory() != Type.KEYSYM || accessor.getBoundKey().getCode() == InputUtil.UNKNOWN_KEY.getCode())
+                continue;
+            keyBinding.setPressed(isKeyPressed(MinecraftKt.getWindow().getHandle(), accessor.getBoundKey().getCode()));
+        }
+    }
+
     @Inject(method = "unpressAll", at = @At("HEAD"), cancellable = true)
     private static void unpressAll(CallbackInfo ci) {
         val screen = ScreenContext.Companion.current();
         if (screen != null && screen.isPassEvents()) {
             ci.cancel();
+        } else {
+            for (KeyBinding keyBinding : KEYS_BY_ID.values()) {
+                var accessor = (KeyBindingAccessor) keyBinding;
+                accessor.setTimesPressed(0);
+                keyBinding.setPressed(false);
+            }
         }
     }
 
--- src/main/java/ru/dargen/evoplus/mixin/render/hud/BossBarHudAccessor.java
+++ src/main/java/ru/dargen/evoplus/mixin/render/hud/BossBarHudAccessor.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 @Mixin(BossBarHud.class)
 public interface BossBarHudAccessor {
 
-    @Accessor
+    @Accessor("bossBars")
     Map<UUID, ClientBossBar> getBossBars();
+
 }
