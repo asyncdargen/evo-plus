878f81794e7e5369343997be1f700836963d4489
Вячеслав <smole17@mail.ru>
2024-04-02 19:54:27 +0300

    added prefixes for on players

--- src/main/java/ru/dargen/evoplus/mixin/entity/player/PlayerEntityMixin.java
+++ src/main/java/ru/dargen/evoplus/mixin/entity/player/PlayerEntityMixin.java
@@ -1,6 +1,7 @@
 package ru.dargen.evoplus.mixin.entity.player;
 
 import com.mojang.authlib.GameProfile;
+import lombok.val;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ru.dargen.evoplus.PrefixParser;
 import ru.dargen.evoplus.util.minecraft.MinecraftKt;
 
 import static net.minecraft.scoreboard.Team.decorateName;
@@ -25,15 +27,12 @@ public abstract class PlayerEntityMixin {
 
     @Shadow
     public abstract Text getName();
-//    @Inject(at = @At("HEAD"), method = "getDisplayName", cancellable = true)
-//    public void getDisplayName(CallbackInfoReturnable<Text> cir) {
-//        var name = getEntityName();
-//
-//        if (gameProfile.getProperties().containsKey("evo_plus")) {
-//            cir.setReturnValue(
-//                    Text.literal("EP ")
-//                            .append(decorateName(MinecraftKt.getWorld().getScoreboard().getPlayerTeam(name), Text.literal(name)))
-//            );
-//        }
-//    }
+    @Inject(at = @At("HEAD"), method = "getDisplayName", cancellable = true)
+    public void getDisplayName(CallbackInfoReturnable<Text> cir) {
+        val prefix = PrefixParser.INSTANCE.getPrefixes().getProperty(getEntityName());
+
+        if (prefix == null || prefix.isEmpty()) return;
+
+        cir.setReturnValue(Text.literal(prefix).append(cir.getReturnValue()));
+    }
 }
