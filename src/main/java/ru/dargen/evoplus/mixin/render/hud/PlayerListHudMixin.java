3c2e580479a0b494c609022d74751fb6f20eff8f
Vyacheslav <47749405+Smole17@users.noreply.github.com>
2024-07-07 02:45:40 +0300

    fixed barrel detector and rollbacked some lost things

---
+++ src/main/java/ru/dargen/evoplus/mixin/render/hud/PlayerListHudMixin.java
@@ -0,0 +1,53 @@
+package ru.dargen.evoplus.mixin.render.hud;
+
+import lombok.val;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.hud.PlayerListHud;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.StringVisitable;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import ru.dargen.evoplus.features.misc.MiscFeature;
+import ru.dargen.evoplus.features.potion.PotionFeature;
+import ru.dargen.evoplus.protocol.EvoPlusProtocol;
+import ru.dargen.evoplus.protocol.registry.PotionType;
+import ru.dargen.evoplus.util.format.TimeKt;
+
+import java.util.List;
+
+@Mixin(PlayerListHud.class)
+public abstract class PlayerListHudMixin {
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;wrapLines(Lnet/minecraft/text/StringVisitable;I)Ljava/util/List;", ordinal = 1))
+    private List<OrderedText> render(TextRenderer instance, StringVisitable text, int width) {
+        val stringBuilder = new StringBuilder();
+        val showServerInTab = MiscFeature.INSTANCE.getShowServerInTab();
+
+        if (PotionFeature.INSTANCE.getEnabledPotionsInTab()) {
+            val potionTimers = PotionFeature.INSTANCE.getPotionTimers();
+            if (!potionTimers.isEmpty()) {
+                stringBuilder.append("\n§e§lÀêòèâíûå Ýôôåêòû §r§8(%s)".formatted(potionTimers.size()));
+                potionTimers.forEach((potionId, potionState) -> {
+                    var type = PotionType.Companion.byOrdinal(potionId);
+                    if (type == null) return;
+
+                    stringBuilder.append("\n%s (%s%%) §f%s".formatted(
+                            type.getDisplayName(),
+                            potionState.getQuality(),
+                            TimeKt.getAsShortTextTime(potionState.getEndTime() - System.currentTimeMillis()))
+                    );
+                });
+                stringBuilder.append("\n");
+            }
+        }
+
+        if (showServerInTab)
+            stringBuilder.append(text.getString())
+                    .append("\nÒåêóùèé ñåðâåð: §e%s".formatted(EvoPlusProtocol.INSTANCE.getServer().toString()));
+
+        text = Text.of(stringBuilder.toString());
+        return instance.wrapLines(text, width);
+    }
+}
--- src/main/kotlin/ru/dargen/evoplus/util/evo/blocks.kt
+++ src/main/kotlin/ru/dargen/evoplus/util/evo/blocks.kt
@@ -2,30 +2,22 @@ package ru.dargen.evoplus.util.evo
 
 import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
-import net.minecraft.block.MushroomBlock
+import net.minecraft.block.NoteBlock
+import net.minecraft.block.enums.Instrument
 
 fun BlockState.isBarrel() = isNormalBarrel() || isNetherBarrel() || isEndBarrel()
 
-fun BlockState.isNormalBarrel() = block == Blocks.MUSHROOM_STEM
-        && get(MushroomBlock.NORTH) == false
-        && get(MushroomBlock.EAST) == false
-        && get(MushroomBlock.SOUTH) == false
-        && get(MushroomBlock.WEST) == false
-        && get(MushroomBlock.UP) == false
-        && get(MushroomBlock.DOWN) == false
+fun BlockState.isNormalBarrel() = block == Blocks.NOTE_BLOCK
+        && get(NoteBlock.INSTRUMENT) === Instrument.FLUTE
+        && get(NoteBlock.NOTE) == 21
+        && get(NoteBlock.POWERED) == false
 
-fun BlockState.isNetherBarrel() = block == Blocks.MUSHROOM_STEM
-        && get(MushroomBlock.NORTH) == true
-        && get(MushroomBlock.EAST) == true
-        && get(MushroomBlock.SOUTH) == false
-        && get(MushroomBlock.WEST) == false
-        && get(MushroomBlock.UP) == false
-        && get(MushroomBlock.DOWN) == false
+fun BlockState.isNetherBarrel() = block == Blocks.NOTE_BLOCK
+        && get(NoteBlock.INSTRUMENT) === Instrument.FLUTE
+        && get(NoteBlock.NOTE) == 18
+        && get(NoteBlock.POWERED) == false
  
-fun BlockState.isEndBarrel() = block == Blocks.MUSHROOM_STEM
-        && get(MushroomBlock.NORTH) == true
-        && get(MushroomBlock.EAST) == true
-        && get(MushroomBlock.SOUTH) == true
-        && get(MushroomBlock.WEST) == true
-        && get(MushroomBlock.UP) == false
-        && get(MushroomBlock.DOWN) == false
+fun BlockState.isEndBarrel() = block == Blocks.NOTE_BLOCK
+        && get(NoteBlock.INSTRUMENT) === Instrument.FLUTE
+        && get(NoteBlock.NOTE) == 16
+        && get(NoteBlock.POWERED) == false
