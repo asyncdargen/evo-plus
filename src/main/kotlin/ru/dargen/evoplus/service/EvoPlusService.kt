5e0a922a9fb24feedf07599c2ed28f12034e3af9
dargen <dargencode@mail.ru>
2024-10-19 19:28:39 +0300

    fix

--- src/main/kotlin/ru/dargen/evoplus/service/EvoPlusService.kt
+++ src/main/kotlin/ru/dargen/evoplus/service/EvoPlusService.kt
@@ -1,47 +1,34 @@
 package ru.dargen.evoplus.service
 
-import feign.Feign
-import feign.gson.GsonDecoder
-import feign.gson.GsonEncoder
-import ru.dargen.evoplus.Logger
-import ru.dargen.evoplus.api.event.evo.ChangeLocationEvent
-import ru.dargen.evoplus.api.event.evo.EvoJoinEvent
-import ru.dargen.evoplus.api.event.on
-import ru.dargen.evoplus.api.scheduler.async
-import ru.dargen.evoplus.api.scheduler.scheduleEvery
-import ru.dargen.evoplus.util.Updater
-import ru.dargen.evoplus.util.collection.takeIfNotEmpty
 import ru.dargen.evoplus.util.minecraft.Client
 import ru.dargen.evoplus.util.newSetCacheExpireAfterWrite
-import java.util.concurrent.TimeUnit.SECONDS
-import java.util.logging.Level
 import kotlin.time.Duration.Companion.minutes
 
 object EvoPlusService {
 
-    private val GameClient = Feign.builder()
-        .encoder(GsonEncoder())
-        .decoder(GsonDecoder())
-        .target(ru.dargen.evoplus.service.controller.GameClient::class.java, "https://evo-plus.dargen.space")
+//    private val GameClient = Feign.builder()
+//        .encoder(GsonEncoder())
+//        .decoder(GsonDecoder())
+//        .target(ru.dargen.evoplus.service.controller.GameClient::class.java, "https://evo-plus.dargen.space")
 
     private val ingamePlayers = newSetCacheExpireAfterWrite<String>(1.minutes)
 
     init {
-        scheduleEvery(period = 30, unit = SECONDS) {
-            runCatching {
-                GameClient.update(Client.session.username, Updater.ModVersion)
-            }.onFailure {
-                Logger.log(Level.SEVERE, "Error while updating ingame status", it)
-            }.onSuccess {
-                if (!it) {
-                    Logger.warning("Failed ingame status update")
-                }
-            }
-        }
-
-        scheduleEvery(10, 10, unit = SECONDS) { fetchIngamePlayers() }
-        on<ChangeLocationEvent> {async { fetchIngamePlayers() } }
-        on<EvoJoinEvent> { async { fetchIngamePlayers() }}
+//        scheduleEvery(period = 30, unit = SECONDS) {
+//            runCatching {
+//                GameClient.update(Client.session.username, Updater.ModVersion)
+//            }.onFailure {
+//                Logger.log(Level.SEVERE, "Error while updating ingame status", it)
+//            }.onSuccess {
+//                if (!it) {
+//                    Logger.warning("Failed ingame status update")
+//                }
+//            }
+//        }
+//
+//        scheduleEvery(10, 10, unit = SECONDS) { fetchIngamePlayers() }
+//        on<ChangeLocationEvent> {async { fetchIngamePlayers() } }
+//        on<EvoJoinEvent> { async { fetchIngamePlayers() }}
     }
 
     fun isIngame(username: String) = username.lowercase() in ingamePlayers
@@ -53,15 +40,15 @@ object EvoPlusService {
             ?.map(String::lowercase)
             ?: emptySet(),
     ) {
-        players.takeIfNotEmpty() ?: return
-        runCatching {
-            GameClient.checkPlayers(players)
-        }.onFailure {
-            Logger.log(Level.SEVERE, "Error while fetch ingame players", it)
-        }.onSuccess {
-            ingamePlayers.addAll(it)
-            Logger.info("${it.size}/${players.size} with EvoPlus!")
-        }
+//        players.takeIfNotEmpty() ?: return
+//        runCatching {
+//            GameClient.checkPlayers(players)
+//        }.onFailure {
+//            Logger.log(Level.SEVERE, "Error while fetch ingame players", it)
+//        }.onSuccess {
+//            ingamePlayers.addAll(it)
+//            Logger.info("${it.size}/${players.size} with EvoPlus!")
+//        }
     }
 
 }
